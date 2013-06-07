09774c5839415c83037550705778aa8a1bc3c7e1
Laurent Eschenauer <laurent@eschenauer.be>
2013-06-07 23:37:01 +0200

    Initial tag detection and EKF implementation

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var autonomy = exports;
 
 exports.StateEstimator = require('./lib/StateEstimator');
+exports.EKF = require('./lib/EKF');
+exports.Camera = require('./lib/Camera');
 
 exports.estimateState = function(client, options) {
     var estimator = new autonomy.StateEstimator(client, options);
