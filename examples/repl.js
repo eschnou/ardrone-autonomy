7699fd9710289e13ee4a2d94cc1dc9757f36fa99
Laurent Eschenauer <laurent@eschenauer.be>
2013-06-18 12:09:33 +0200

    Improving the PID Controller
    This is a WIP, the controller needs more work,
    but it is slowly getting there :-)
    
    - Filter background noise in EKF
    - Decouple goals (set goal per dof leaving others floating)
    - Exeute control on navdata (to reduce delay)
    - Improved PID coefficient (need more work)

--- examples/repl.js
+++ examples/repl.js
@@ -1,9 +1,30 @@
+var fs = require('fs');
+var path = require('path');
+var df = require('dateformat');
 var arDrone = require('ar-drone');
 var autonomy = require('..');
 
 var client  = arDrone.createClient();
-var ctrl = new autonomy.Controller(client);
-var repl = client.createRepl();
+var ctrl    = new autonomy.Controller(client, {debug: false});
+var repl    = client.createRepl();
 
+// Add a ctrl object to the repl. You can use the controller
+// from there. E.g.
+// ctrl.go({x:1, y:1});
+//
 repl._repl.context['ctrl'] = ctrl;
+
+// Log navdata and estimated state - for debugging
+//
+// var folder = df(new Date(), "yyyy-mm-dd_hh-MM-ss");
+// fs.mkdir(path.join('/tmp', folder), function() {
+//    navStream = fs.createWriteStream(path.join('/tmp', folder, 'navdata.txt'));
+//    stateStream = fs.createWriteStream(path.join('/tmp', folder, 'state.txt'));
+// });
+
+//client.on('navdata', function(data) {
+//    navStream.write(JSON.stringify(data) + "\n");
+//    stateStream.write(JSON.stringify(ctrl.state()) + "\n");
+//});
+
 
--- tests/simple.js
+++ tests/simple.js
@@ -1,10 +1,13 @@
+// TODO This is broken. Need to write real tests.
+console.log("The test is broken. Will have to write proper tests :-)");
+
 var autonomy    = require('..');
 var client      = require('./mock/client');
 
-var controller  = new autonomy.Controller(new client(), {state: {x: 0, y:0, z:1, yaw: Math.PI/4}, debug: true});
+var controller  = new autonomy.Controller(new client(), {state: {x: 0, y:0, z:1, yaw: 0}, debug: false});
 
 console.log("State: %j", controller.state());
 
-controller.go({x: 0, y: 0, z:1, yaw: -45}, function(state) {
+controller.go({x: 1, y: 1}, function(state) {
     console.log("Reached state %j", state);
 });
